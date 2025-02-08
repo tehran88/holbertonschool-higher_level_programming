311a22f98b0775009f142c7439be562e400a5667
tehran88 <tehranabilovaa@gmail.com>
2025-02-08 09:41:12 +0000

    inheritance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines MyList, a subclass of list, with additional functionality to
+print sorted lists"""
+
+
+class MyList(list):
+    """MyList inherits from the built-in list class, adding a method to print
+    the list sorted"""
+
+    def print_sorted(self):
+        """Prints the elements of the list sorted in ascending order without
+        altering the original list"""
+        print(sorted(self))
+        return sorted(self)
