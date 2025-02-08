aeb2571121af088072cb0e6f49329fd2eee1fccb
tehran88 <tehranabilovaa@gmail.com>
2025-02-08 09:47:18 +0000

    inheritance

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Determines if an object is an instance of a subclass of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherits from
+    the specified class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class,
+        otherwise False"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
